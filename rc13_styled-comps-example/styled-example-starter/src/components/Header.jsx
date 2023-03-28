12daa2d9a33aab1357108ff7e94e6b5ffb2742a0
SametErdogan67 <samedi6774@gmail.com>
2023-03-28 22:40:14 +0300

    style component example v1

---
+++ rc13_styled-comps-example/styled-example-starter/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "./styled/Button";
+import Nav from "./styled/Nav";
+
+const Header = () => {
+  return (
+    <div>
+      <Nav>
+        <div>
+          <img src="./images/logo.png" alt="logo" width={"300px"} />
+        </div>
+        <div>
+          <Button color="#AF3A53">Apply Courses</Button>
+          <Button bg="#AF3A53">Talk to Advisor</Button>
+        </div>
+      </Nav>
+    </div>
+  );
+};
+
+export default Header;
