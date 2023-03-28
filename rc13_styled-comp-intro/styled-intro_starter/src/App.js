0ad471d300ad782deeec1ac116d9c4866064a0a4
SametErdogan67 <samedi6774@gmail.com>
2023-03-28 21:17:39 +0300

    styled comp1

--- rc13_styled-comp-intro/styled-intro_starter/src/App.js
+++ rc13_styled-comp-intro/styled-intro_starter/src/App.js
@@ -1,10 +1,13 @@
-import { Button } from "./components/Buton.styled";
+import { Button, FS13Button } from "./components/Buton.styled";
 
 const App = () => {
   return (
     <>
       <h1>Styled Components</h1>
       <Button>Send</Button>
+      <Button primary>Send</Button>
+      <FS13Button>Gone</FS13Button>
+      <FS13Button react>Gone</FS13Button>
     </>
   );
 };
--- rc13_styled-comp-intro/styled-intro_starter/src/components/Buton.styled.jsx
+++ rc13_styled-comp-intro/styled-intro_starter/src/components/Buton.styled.jsx
@@ -1,10 +1,26 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  background-color: hotpink;
-  color: white;
+  /* background-color: hotpink;
+  color: white; */
+
+  background-color: ${(props) => (props.primary ? "white" : "hotpink")};
+  color: ${({ primary }) => (primary ? "hotpink" : "white")};
   width: 8rem;
   padding: 1rem 2rem;
   border-radius: 1rem;
+  border: 2px solid hotpink;
   border-color: hotpink;
+  margin-right: 10px;
+  font-size: 1.1rem;
+  &:hover {
+    transform: scale(0.95);
+  }
+`;
+
+export const FS13Button = styled(Button)`
+  background-color: ${({ react }) => (react ? "gray" : "white")};
+  color: ${({ react }) => (react ? "white" : "gray")};
+  border: 2px solid gray;
+  border-color: ${({ react }) => (react ? "white" : "gray")};
 `;
